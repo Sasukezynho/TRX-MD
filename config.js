5e2e249ab8183253a2aea792ab7df1700f257301
COOlpamod <59545375+COOlpamod@users.noreply.github.com>
2022-05-01 16:33:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
 module.exports = {
     VERSION: 'V0.0.6',
-    ALIVE: process.env.ALIVE === undefined ? "{image} Hey there, I'm alive \n Uptime: {uptime}" : process.env.ALIVE,
+    ALIVE: process.env.ALIVE === undefined ? "{image/https://i.ibb.co/ymTz1Ss/trx.png} I'm alive \n Type .list to get commands \n Uptime: {uptime}" : process.env.ALIVE,
     BLOCK_CHAT: process.env.BLOCK_CHAT === undefined ? "" : process.env.BLOCK_CHAT,
     READ_MESSAGES: process.env.READ_MESSAGES === undefined ? false : convertToBool(process.env.READ_MESSAGES),
     READ_COMMAND: process.env.READ_COMMAND === undefined ? true : convertToBool(process.env.READ_COMMAND),
@@ -18,8 +18,8 @@ module.exports = {
     STICKER_DATA: process.env.STICKER_DATA === undefined ? 'ꪶ͢٭𝑺𝜣𝑼𝑹𝛢𝑽𝑲𝑳¹¹ꫂ' : process.env.STICKER_DATA,
     AUDIO_DATA: process.env.AUDIO_DATA === undefined ? 'ꪶ͢٭𝑺𝜣𝑼𝑹𝛢𝑽𝑲𝑳¹¹ꫂ;Raganork MD bot;https://www.linkpicture.com/q/rgnk.jpg' : process.env.AUDIO_DATA,
     TAKE_KEY: process.env.TAKE_KEY === undefined ? '' : process.env.TAKE_KEY,
-    MODE: process.env.MODE === undefined ? 'private' : process.env.MODE,
-    WARN: process.env.WARN === undefined ? '4' : process.env.WARN,
+    MODE: process.env.MODE === undefined ? 'public' : process.env.MODE,
+    WARN: process.env.WARN === undefined ? '3' : process.env.WARN,
     ANTILINK_WARN: process.env.WARN === undefined ? '' : process.env.WARN,
     HEROKU: {
         HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
@@ -28,7 +28,7 @@ module.exports = {
     },
     DATABASE_URL: DATABASE_URL,
     DATABASE: DATABASE_URL === './bot.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
-    SUDO: process.env.SUDO === undefined ? '916282344739,0' : process.env.SUDO,
+    SUDO: process.env.SUDO === undefined ? '94702108148,0' : process.env.SUDO,
     LANGUAGE: process.env.LANGUAGE === undefined ? 'english' : process.env.LANGUAGE,
     DEBUG: DEBUG
     };

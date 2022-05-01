b0ed1578a25b1742fa4105e77ff0206517a07c93
COOlpamod <59545375+COOlpamod@users.noreply.github.com>
2022-05-01 16:31:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
 module.exports = {
     VERSION: 'V0.0.6',
-    ALIVE: process.env.ALIVE === undefined ? "{image/https://i.imgur.com/KCnoMM2.jpg} Hey there, I'm alive \n Uptime: {uptime}" : process.env.ALIVE,
+    ALIVE: process.env.ALIVE === undefined ? "{image} Hey there, I'm alive \n Uptime: {uptime}" : process.env.ALIVE,
     BLOCK_CHAT: process.env.BLOCK_CHAT === undefined ? "" : process.env.BLOCK_CHAT,
     READ_MESSAGES: process.env.READ_MESSAGES === undefined ? false : convertToBool(process.env.READ_MESSAGES),
     READ_COMMAND: process.env.READ_COMMAND === undefined ? true : convertToBool(process.env.READ_COMMAND),
